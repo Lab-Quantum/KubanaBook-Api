3026af3c7f6bf6828dd6c4fb1a8a3ec8dd2bed9f
Luan Peil <luan@peil.dev>
2020-02-24 14:13:17 -0300

    First Commit

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+if(session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$data = json_decode(file_get_contents("php://input"));
